983051ced4216aa89a86b04e660eae67fdbdf6d0
Nero25-3 <brunocoya25@gmail.com>
2025-10-31 21:40:47 +0100

    fix main.go

--- cmd/main.go
+++ cmd/main.go
@@ -1,8 +1,8 @@
 package main
 
-import "CLI_go_monitor/internal/core"
+import "CLI_go_monitor/internal/plugins"
 
 // main is the entry point of the application.
 func main() {
-	core.Execute()
+	plugins.Execute()
 }
